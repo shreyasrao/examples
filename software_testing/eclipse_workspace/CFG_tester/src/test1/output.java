f3510b08e53ceecab5643f378373c487e02520cb
shreyasrao <s23rao@gmail.com>
2016-04-25 23:16:57 -0500

    Package other: has the tested prime path coverage tool implemented.

---
+++ software_testing/eclipse_workspace/CFG_tester/src/test1/output.java
@@ -0,0 +1,39 @@
+package test1;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.FileInputStream;
+import java.security.MessageDigest;
+import java.util.*;
+import java.io.File;
+import java.io.PrintWriter;
+
+
+// this is a comenet
+
+public class output {
+	
+	int max(int x, int y) {
+			System.out.println(0);
+		if (x < y) {
+			System.out.println(1);
+		} 
+		else {
+			System.out.println(2);
+		}
+			return 0; //System.out.println(3);
+	}
+	int down(int a) {
+			System.out.println(4);
+		if (a<5){ // print 5
+			System.out.println(5);
+			for (int i=0;i<10;i++){
+			System.out.println(7);
+			}
+		}
+		System.out.println('s');return 0; //un-named
+	}
+}
