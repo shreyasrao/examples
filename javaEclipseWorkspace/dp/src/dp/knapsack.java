8a4341303bd8834d499a0eced5a37c07156bba22
shreyasrao <s23rao@gmail.com>
2017-06-26 13:02:48 -0700

    Added divide the spoils algo to knapsack.java. Added a couple tests for the method into tester file

--- javaEclipseWorkspace/dp/src/dp/knapsack.java
+++ javaEclipseWorkspace/dp/src/dp/knapsack.java
@@ -6,12 +6,6 @@ import java.util.List;
 
 public class knapsack{
 	
-	/*
-	 * Classic knapsack dynamic programming problem
-	 * solution uses O(mn) space with O(mn) runtime
-	 * where n is number of items and m is max capacity (i.e. weight)
-	 * */
-	
 	public static class item{
 		int weight, value;
 		
@@ -22,6 +16,13 @@ public class knapsack{
 	}
 	
 	public static int getMax(List<item> A, int capacity) {
+		
+		/*
+		 * Classic knapsack dynamic programming problem
+		 * solution uses O(mn) space with O(mn) runtime
+		 * where n is number of items and m is max capacity (i.e. weight)
+		 * */
+		
 		List<List<Integer>> M = new ArrayList<List<Integer>>();
 		for(int i=0;i<A.size();i++) {
 			M.add(new ArrayList<Integer>(Collections.nCopies(capacity+1, -1)));
@@ -44,6 +45,50 @@ public class knapsack{
 			}
 		}
 		return M.get(M.size()-1).get(M.get(0).size()-1);
+	}
+	
+	public static int divideSpoils(int[] A) {
+		/*
+		 * Given array of item values, split them into 2 subset
+		 * such that the difference between sub set sums is as
+		 * small as possible
+		 * Return gives sum of largest subset <= sum/2;
+		 * Trivially the other subset is sum - return val
+		 * */
+		
+		int N = A.length;
+		int sum = 0;
+		for(int a:A) sum += a;
+		
+		//find the target sum by ceil(sum/2)
+		sum = (sum%2==0) ? sum/2 : sum/2 + 1;
+		boolean M[][] = new boolean[N+1][sum+1];
+		
+		// when sum=0, all n values are true
+		for(int i=0;i<N+1;i++) M[i][0] = true;
+		
+		// when sum!=0 and n!=0, all values are false
+		for(int j=1;j<sum+1;j++) M[0][j] = false;
+		
+		for(int n=1;n<N+1;n++) {
+			
+			for(int s=1;s<sum+1;s++) {
+				boolean m = M[n-1][s]; //exclude current element 				
+				//if current s gte to element, then consider including element
+				M[n][s] = (s>=A[n-1]) ? m || M[n-1][s-A[n-1]] : m ;
+			}
+			
+		}
+		
+		// once we have filled M, traverse to find largest s thats true
+		int res=0;
+		for(int i=sum;i>=0;i--) {
+			if(M[N][i]) {
+				res = i;
+				break;
+			}
+		}
+		return res;
 	}
 	
 }
--- javaEclipseWorkspace/dp/src/dp/knapsackTester.java
+++ javaEclipseWorkspace/dp/src/dp/knapsackTester.java
@@ -3,6 +3,8 @@ package dp;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import org.junit.Test;
@@ -20,5 +22,21 @@ public class knapsackTester{
 		assertEquals(80,knapsack.getMax(A,5));
 	}
 	
+	@Test
+	public void split() {
+		int[] set = {5,6,3,5};
+		int a = knapsack.divideSpoils(set);
+		//for given input, split is 10 and 9
+		//our algo should return 10 as sum of one of the subsets
+		assertEquals(a,10);
+	}
 	
+	@Test
+	public void split1() {
+		int[] set = {65,35,245,195,65,150,275,155,120,320,75,40,200,100,220,99};
+		int a = knapsack.divideSpoils(set);
+		//for given input, split is 1180 and 1179
+		//our algo should return 1180 as sum of one of the subsets
+		assertEquals(a,1180);
+	}
 }
