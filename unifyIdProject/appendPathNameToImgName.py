468918e3ecba8c23f4a7132b5ad728565fdfcd08
shreyasrao <s23rao@gmail.com>
2017-08-04 02:20:21 -0700

    initial commit of documentation. AppendPathName script is added. <folder of images> command line arg will add folderName/subject/type/ discriptor to filename

---
+++ unifyIdProject/appendPathNameToImgName.py
@@ -0,0 +1,26 @@
+import os
+import sys, re
+
+subjectlist = [];
+print("Changing all of the img names located in : " + sys.argv[1])
+
+
+for root,subjects,notUsed in os.walk(sys.argv[1], topdown=False):
+	for dirName in subjects:
+		subjectlist.append(os.path.join(root,dirName))
+
+for subjectTypeDir in subjectlist:
+	#print(subjectTypeDir)
+	for root,notUseddirs,files in os.walk(subjectTypeDir):
+		prefix = subjectTypeDir.replace('/','-')
+		for f in files:
+			#print(prefix+f)	
+			origF = os.path.join(root,f)		
+			#print("rename:" + origF)		
+			newFileName = origF.replace('/','-')
+			pathToImg = root + "/"
+			#print("place in:"+ pathToImg)
+			#print("nuname:" + pathToImg+ newFileName)
+			newName = pathToImg + newFileName
+			os.rename(origF,newName)
+		
